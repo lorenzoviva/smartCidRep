7ca1575253a6b585da06af1231933dbc28620b4e
lorenzoviva <lorenzo.vaccaro@hotmail.com>
2014-03-27 00:55:23 +0100

    quasi finite le trasformazioni sulla main_activity, ora funziona ma non ho ancora limitato le traslazioni dell'immagine

--- projectCid/src/com/projectCid/smartcid/MainActivity.java
+++ projectCid/src/com/projectCid/smartcid/MainActivity.java
@@ -2,20 +2,15 @@
 package com.projectCid.smartcid;
 
 import android.os.Bundle;
-import android.animation.ObjectAnimator;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
-import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnDragListener;
 import android.view.View.OnTouchListener;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
@@ -33,14 +28,23 @@ public class MainActivity extends Activity {
 	ImageView img;
 
 	float aspectRatio = 1;// rapporto di dimensioni tra la grandezza
-					// dell'immagine del modulo CID
 
+	// dell'immagine del modulo CID
 	// originale (pieno schermo) e le dimensioni dell'immagine rappresentata
 	// graficamente
-	float vx = 0, vy = 0, x = 0, y = 0;
-	ScaleAnimation scale= new ScaleAnimation(1,	1, 1, 1);
+	float lastX = 0, lastY = 0, relativeX = 0, relativeY = 0, absoluteX = 0, absoluteY = 0;//variabili d'appoggio per la traslazione dell'immagine
+	//lastX rappresenta la cordinata X dell'ultima posizione del cursore nell' occorrenza :
+	//-lo schermo è stato appena toccato (vedi onTouch ACTION_DOWN)
+	//-l'utente muove il dito sullo schermo (vedi onTouch ACTION_MOVE)
+	//relativeX rappresenta la differenza tra le cordinate X dell'ultima posizione del cursore e quella corrente
+	//absoluteX rappresenta la coordinata X della posizione assoluta dell'immagine nell'activity
+		
+	ScaleAnimation scale = new ScaleAnimation(1, 1, 1, 1);
+
 	TranslateAnimation trans = new TranslateAnimation(0, 0, 0, 0);
-	Animation ani ; 
+
+	AnimationSet set;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
@@ -50,23 +54,18 @@ public class MainActivity extends Activity {
 		username = intent.getStringExtra(EXTRA_MESSAGE);
 		zoom = (ZoomControls) findViewById(R.id.zoomControls1);
 		img = (ImageView) findViewById(R.id.imageView1);
-		zoom.setOnZoomInClickListener(new OnClickListener() {
 		
+
+		zoom.setOnZoomInClickListener(new OnClickListener() {
+
 			public void onClick(View v) {
-				
+
 				aspectRatio += 0.1f;
 				if (aspectRatio > 5) {
 					aspectRatio = 5;
 				}
-				
-				
-				scale = new ScaleAnimation(aspectRatio,	aspectRatio, aspectRatio, aspectRatio);
-				scale.setDuration(0); // Make animation instant
-				scale.setFillAfter(true); // Tell it to persist after the animation ends
-				
-				img.startAnimation(trans);
-				img.startAnimation(scale);
-				
+
+				muoviImmagine();
 
 			}
 		});
@@ -79,16 +78,8 @@ public class MainActivity extends Activity {
 					aspectRatio = 1;
 				}
 
-				scale = new ScaleAnimation(aspectRatio,	aspectRatio, aspectRatio, aspectRatio);
-				scale.setDuration(0); // Make animation instant
-				scale.setFillAfter(true); // Tell it to persist after the
-											// animation ends
-				Log.i("mainActivity", "touch event");
-				
-				
-				img.startAnimation(trans);
-				img.startAnimation(scale);
-				
+				muoviImmagine();
+
 			}
 		});
 		img.setOnTouchListener(new OnTouchListener() {
@@ -96,37 +87,30 @@ public class MainActivity extends Activity {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 
-				
 				if (event.getAction() == MotionEvent.ACTION_DOWN) {
-					vx = event.getX();
-					vy = event.getY();
+					lastX = event.getX();
+					lastY = event.getY();
+
 				} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-					x = event.getX() - vx;
-					y = event.getY() - vy;
-					trans = new TranslateAnimation(x, x, y, y);
-					trans.setDuration(0); // Make animation instant
-					trans.setFillAfter(true); // Tell it to persist after the
-												// animation ends
-					img.startAnimation(scale);
-					img.startAnimation(trans);
+					relativeX = (event.getX() - lastX);//la differenza è la X del vettore spostamento della traslazione relativa da applicare all'immagine
+					relativeY = (event.getY() - lastY);
+					absoluteX += relativeX;//sommo i vettori 
+					absoluteY += relativeY;
+					lastX = event.getX();
+					lastY = event.getY();
+
+				}
+				else if (event.getAction() == MotionEvent.ACTION_UP) {
 
 				}
 
+				muoviImmagine();
 				return true;
 			}
 
 		});
-		/*
-		 * img.setOnDragListener(new OnDragListener(){
-		 * 
-		 * @Override public boolean onDrag(View v, DragEvent event) { // TODO
-		 * Auto-generated method stub img.setX(img.getX()+event.getX());
-		 * img.setY(img.getY()+event.getY()); return true; }
-		 * 
-		 * });
-		 */
+		
 	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -135,11 +119,24 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	public void zoomin(View view) {
+	private void muoviImmagine() {
 
+		scale.setDuration(0);// imposta la durata dell'animazione di ridimensionamento a 0 millisecondi (istantanea)
+		scale.setFillAfter(true); //impone all'animazione di ridimensionamento di perdurare anche dopo l'animazione grafica
+		scale = new ScaleAnimation(aspectRatio, aspectRatio, aspectRatio, aspectRatio);//crea l'oggeto di ridimensionamento
+		trans.setDuration(0);// imposta la durata dell'animazione di traslazione a 0 millisecondi (istantanea)
+		trans.setFillAfter(true);//impone all'animazione di traslazione di perdurare anche dopo l'animazione grafica
+		trans = new TranslateAnimation(absoluteX, absoluteX, absoluteY, absoluteY);//crea l'oggeto di traslazione
+		set = new AnimationSet(true);//creo un oggetto che permette di eseguire in sequenza piu animazioni 
+		set.addAnimation(new TranslateAnimation(-img.getWidth()/2,-img.getWidth()/2,-img.getHeight()/2,-img.getHeight()/2));//attraverso questa istruzione e quella subuto dopo la prossima, si ottiene uno zoom che punta il centro dell'immagine
+		set.addAnimation(scale);//aggiunge nella sequenza l'animazione di ridimensionamento
+		set.addAnimation(new TranslateAnimation(img.getWidth()/2,img.getWidth()/2,img.getHeight()/2,img.getHeight()/2));
+		set.addAnimation(trans);//aggiunge nella sequenza l'animazione di ridimensionamento
+		set.setFillAfter(true);//impone al set di animazioni di perdurare anche dopo l'animazione grafica
+		img.startAnimation(set);
 	}
 
-	public void zoomout(View view) {
 
-	}
+
+
 }
